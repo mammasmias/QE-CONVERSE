ae6e7fb2316ecc62db2eab449151ccf53c728a16
SFioccola <fioccola.simone@gmail.com>
2024-07-31 15:16:05 +0200

    add benchmarking

---
+++ benchmarking/PbF/g-tensor_script.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+NAME="pbf_gtensor"
+
+for DIR in 2 3 
+do
+cat > ${NAME}_${DIR}.in << EOF
+
+&input_qeconverse
+        prefix = 'PbF'
+        outdir = './scratch/'
+        diagonalization = 'david'
+        verbosity = 'high'
+        q_gipaw = 0.01
+        dudk_method = 'covariant'
+        diago_thr_init = 1d-4
+        conv_threshold = 1d-8
+        mixing_beta = 0.5
+        lambda_so(${DIR}) = 1.0
+/
+EOF
+
+mpirun -np 6  ../../QE-CONVERSE/bin/qe-converse.x < ${NAME}_${DIR}.in > ${NAME}_${DIR}.out
+echo ${NAME}_${DIR}
+
+done
+
---
+++ benchmarking/README.md
@@ -0,0 +1,6 @@
+This folder contains the PbF family radicals used to benchmaring the QE-CONVERSE code which the results are presented 
+in "Benchmaring test" paragraph of the paper.
+
+NOTE: in order to decrease the computation effort we have reduced the cubic cell volume to 1185 Ang^3 against the 8000 Ang^3 reported in the paper.
+      So, the results here might be slightly different from the Table 1. 
+
