92613e020a488e0dde53c4fed9c9fa730bf14aea
SFioccola <131052903+SFioccola@users.noreply.github.com>
2025-01-19 11:16:05 +0100

    Fermi energy buf fixed

--- src/compute_dudk_new.f90
+++ src/compute_dudk_new.f90
@@ -552,9 +552,10 @@ SUBROUTINE find_nbnd_occ(ik, nbnd_occ, emin, emax)
   real(dp), intent(out) :: emin, emax
   real(dp) :: small, xmax, fac, e_target
   integer :: ibnd
-
-  IF ( two_fermi_energies ) THEN
+  IF ( lgauss .or. ltetra ) THEN ! metallic case
+    IF ( two_fermi_energies ) THEN
      ef = (ef_up + ef_dw) / 2
+    ENDIF
   ENDIF
 
   if (lgauss) then ! metallic case
